8e48ad75d815586fd980c585e655612d96bc179e
Michael Chisholm <chisholm@mitre.org>
2017-01-05 13:52:47 -0500

    Added some more tests to the pattern-level and/or test suite. Switched off verbose matching in one spot, which I'd left on accidentally.

--- pattern_matcher/test/test_pattern_and_or.py
+++ pattern_matcher/test/test_pattern_and_or.py
@@ -43,18 +43,21 @@ _timestamps = [datetime.datetime.now(dateutil.tz.tzutc())] * len(_observations)
 @pytest.mark.parametrize("pattern", [
     "[person:name='alice'] and [person:age>20]",
     "[person:name='alice'] or [person:name='carol']",
-    "[person:name='alice'] or [person:name='zed']",
+    "[person:name='alice'] or [person:name='zelda']",
+    "[person:age>10] or [person:name='bob'] or [person:name>'amber']",
     # tests operator precedence
     "[person:age > 20] or [person:name > 'zelda'] and [person:age < 0]"
 ])
 def test_and_or_match(pattern):
-    assert match(pattern, _observations, _timestamps, True)
+    assert match(pattern, _observations, _timestamps)
 
 
 @pytest.mark.parametrize("pattern", [
-    "[person:name='alice'] and [person:name='zed']",
-    "[person:name='mary'] or [person:name='zed']",
-    "[person:age > 70] or [person:name > 'zelda'] and [person:name matches /^...?$/]"
+    "[person:name='alice'] and [person:name='zelda']",
+    "[person:name='mary'] or [person:name='zelda']",
+    "[person:age > 70] or [person:name > 'zelda'] and [person:name matches /^...?$/]",
+    # same as precedence test above, with parentheses to alter eval order
+    "([person:age > 20] or [person:name > 'zelda']) and [person:age < 0]"
 ])
 def test_and_or_nomatch(pattern):
     assert not match(pattern, _observations, _timestamps)
