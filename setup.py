9c50537cd12a3629c9955c50fe3f5adc32ce9933
Greg Back <gback@mitre.org>
2017-01-06 10:33:10 -0600

    Add Trove classifiers

--- setup.py
+++ setup.py
@@ -20,4 +20,14 @@ setup(
             'stix2-matcher = stix2matcher.matcher:main',
         ],
     },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
