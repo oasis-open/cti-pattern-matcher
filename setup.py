f2cb313dc6aef027ee99ef874d5072559108f46a
Joshua <jweiss@mitre.org>
2022-07-29 10:52:49 -0400

    added py310 to test harness removed py36

--- setup.py
+++ setup.py
@@ -34,9 +34,9 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
